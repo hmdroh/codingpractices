0d88816064c93f06d1e2ce591601b73a0fb5927c
Hamed Rohani <hamedrohani@MrRs-MacBook-Pro.local>
2019-01-04 00:30:34 -0800

    fizzbuzz es6

---
+++ practice03/solved.js
@@ -0,0 +1,12 @@
+let runfizzBuzz = () => {
+    for(var i = 1; i <= 100; i++){
+        
+        console.log((i % 3 === 0 && i % 5 === 0) ?
+                      "fizzbuzz" : (i % 5 === 0 ?
+                        "buzz" : (i % 3 === 0 ?
+                          "fizz" : i)));
+
+      }
+  };
+  //testing
+  runfizzBuzz();
