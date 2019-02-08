eb22434b4e7f4267c5a3a9a4dc5d0f9a8e9e278c
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-02-07 23:34:45 -0800

    added data structure

---
+++ practice05 - data structures/stacks.js
@@ -0,0 +1,11 @@
+// stacks are used to put things on each other:
+
+
+function stuck (){
+    this.top = 0;
+    this.push = push;
+    this.pop = pop;
+    this.peek = peek;
+    this.clear = clear;
+    
+}
