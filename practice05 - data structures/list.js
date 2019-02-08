beeeee04ac14456088700169a38332e57fb45403
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-02-08 00:06:44 -0800

    list update

--- practice05 - data structures/list.js
+++ practice05 - data structures/list.js
@@ -52,3 +52,78 @@ function remove(element){
 function length(){
     return this.listSize;
 }
+
+function toString(){
+    return this.dataStore;
+}
+
+function insert(element, after){
+    var insPos = this.find(after);
+    if(insPos != -1){
+        this.dataStore.splice(insPos+1, 0, element);
+        this.listSize++;
+        return true;
+    }
+    return false;
+}
+
+function clear(){
+    delete this.dataStore;
+    this.listSize = 0;
+    this.dataStore = [];
+
+}
+
+function contains(element){
+    for(var i =0; i< this.dataStore.length; i++){
+        if(this.dataStore[i] == element){
+            return true;
+        }
+    }
+    return false;
+}
+
+function moveTo(position){
+    this.pos = position;
+}
+
+function getElement(){
+    return this.dataStore[this.pos];
+}
+
+
+function previous(){
+    return this.dataStore[--this.pos];
+}
+
+function next(){
+    return this.dataStore[this.pos++];
+}
+
+function hasNext(){
+    if(this.pos > this.listSize -1){
+        return false;
+    }else {
+        return true;
+    }
+}
+
+function hasPrevious(){
+    if(this.pos <= 0){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function front(){
+    this.pos = 0;
+}
+
+function end(){
+    this.pos = this.listSize -1;
+}
+
+function currPos(){
+    return pos;
+}
