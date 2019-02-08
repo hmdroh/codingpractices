8d703dccd993ef870897f5519883649fe6cd9988
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-02-07 23:44:07 -0800

    final stack

--- practice05 - data structures/stacks.js
+++ practice05 - data structures/stacks.js
@@ -1,11 +1,50 @@
 // stacks are used to put things on each other:
 
 
-function stuck (){
+function Stack (){
     this.top = 0;
     this.push = push;
+    //lets add data as empty
+    this.dataStorage = [];
     this.pop = pop;
     this.peek = peek;
     this.clear = clear;
-    
+    this.length = length;
 }
+
+function push(element){
+    this.dataStorage[this.top++] = element;
+}
+
+function pop(){
+    return this.dataStorage[--this.top];
+}
+
+function peek(){
+    return this.dataStorage[this.top-1];
+}
+
+function clear(){
+    this.top = 0;
+    this.dataStorage.length = 0;
+}
+
+function length(){
+    return this.top;
+}
+
+var x = new Stack();;
+
+x.push("Hamed");
+x.push("Khalid");
+x.push("2019");
+
+console.log(x.pop()); // return 2019
+console.log(x.peek()); // return Khalid
+
+console.log(x.length()); // 2
+
+x.clear(); 
+console.log(x.peek()); // undefined
+
+console.log(x.top); // 0
