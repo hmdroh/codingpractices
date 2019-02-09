16b3c7b7cfd1db5e74cb63005939ba40eccb6aad
Hamed Rohani <hamedrohani@MrRs-MBP.attlocal.net>
2019-02-09 09:40:22 -0800

    example1

--- practice05 - data structures/dictionary.js
+++ practice05 - data structures/dictionary.js
@@ -7,6 +7,8 @@ function Dictionary(){
     this.find = find;
     this.remove = remove;
     this.showAll = showAll;
+    this.count = count;
+    this.clear = clear;
 }
 
 function add(key,value){
@@ -29,6 +31,20 @@ function showAll(){
     }
 }
 
+function count(){
+    var n = 0;
+    for (var key in this.dataStore){
+        ++n;
+    }
+    return n;
+}
+
+function clear(){
+    for(var key in this.dataStore){
+        delete this.dataStore[key];
+    }
+}
+
 /// example 1:
 
 var pbook = new Dictionary();
@@ -40,4 +56,13 @@ pbook.add("Germany", "0049");
 
 console.log("Afghanistan's code is: " + pbook.find("Afghanistan"));
 pbook.remove("UAE");
+
+console.log("Number of Entries: " + pbook.count());
+
 pbook.showAll();
+pbook.clear(); 
+console.log("Number of entries: " + pbook.count());
+
+
+/// adding and sorting to dictionary class:
+
